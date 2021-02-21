5260d4c3f27f69f10ebd3ba00ecdab3c93e42f33
Sunil <sunilnaths@gmail.com>
2021-02-20 16:47:17 -0800

    new file

--- monitor/sh_ip_int.py
+++ monitor/sh_ip_int.py
@@ -1,6 +1,6 @@
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
-from nornir_netmiko.tasks import netmiko_send_command, netmiko_send_config
+from nornir_netmiko.tasks import netmiko_send_command
 from user_login import user_login
 from nornir.core.filter import F
 import ipdb
@@ -14,18 +14,8 @@ def version_task(task):
     r = task.run(netmiko_send_command,
                  command_string=" show ip int br | inc down ")
     task.host['facts'] = r.result
-    #result1 = task.host['facts']['cdp']['index']
-    #for i in result1:
-    #    local_inter = result1[i]['local_interface']
-    #    remote_port = result1[i]['port_id']
-    #    remote_id = result1[i]['device_id']
-    #    cdp_config = task.run(netmiko_send_config, name="CDP Config",
-        #                      config_commands=[
-        #                        f"interface {local_inter}",
-        #                        f"description {remote_id} on {remote_port}"
-        #                        ])
 
 
 sh_version = task1.run(task=version_task)
 print_result(sh_version)
-#ipdb.set_trace()
+
